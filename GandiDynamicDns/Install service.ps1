5b556e4c4fbe95c96a4d45320fd94ee4eefd7343
Ben Hutchison <ben@aldaviva.com>
2025-02-11 13:59:53 -0800

    When installing Windows service, make it auto-restart the process if it crashes

--- GandiDynamicDns/Install service.ps1
+++ GandiDynamicDns/Install service.ps1
@@ -1,3 +1,4 @@
 $binaryPathName = Resolve-Path(join-path $PSScriptRoot "GandiDynamicDns.exe")
 
 New-Service -Name "GandiDynamicDns" -DisplayName "Gandi Dynamic DNS" -Description "Keep a Gandi DNS A record updated with this computer's public IP address." -BinaryPathName $binaryPathName.Path -DependsOn Tcpip
+sc.exe failure GandiDynamicDns actions= restart/60000/restart/60000/restart/60000 reset= 3600
