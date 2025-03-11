83a309d07971c1001e6b403cff760ce099db2cf3
Ben Hutchison <ben@aldaviva.com>
2025-03-11 12:24:46 -0700

    Revert removal of Windows TFM, which I forgot was required for Event Log

--- GandiDynamicDns/DynamicDnsService.cs
+++ GandiDynamicDns/DynamicDnsService.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Net;
 using ThrottleDebounce;
 using Unfucked;
+using Unfucked.HTTP.Exceptions;
 using Unfucked.STUN;
 
 namespace GandiDynamicDns;
@@ -18,6 +19,8 @@ public interface DynamicDnsService: IDisposable {
 public class DynamicDnsServiceImpl(ILiveDns liveDns, ISelfWanAddressClient stun, IOptions<Configuration> configuration, ILogger<DynamicDnsServiceImpl> logger, IHostApplicationLifetime lifetime)
     : BackgroundService, DynamicDnsService {
 
+    private static readonly TimeSpan ONE_SHOT_MODE = TimeSpan.Zero;
+
     public IPAddress? selfWanAddress { get; private set; }
 
     private readonly EventLog? eventLog =
@@ -28,27 +31,44 @@ public class DynamicDnsServiceImpl(ILiveDns liveDns, ISelfWanAddressClient stun,
 #endif
 
     protected override async Task ExecuteAsync(CancellationToken ct) {
-        await Retrier.Attempt(async _ => {
-                if ((await liveDns.Get(RecordType.A, configuration.Value.subdomain, ct))?.Values.First() is { } existingIpAddress) {
-                    try {
-                        selfWanAddress = IPAddress.Parse(existingIpAddress);
-                    } catch (FormatException) { }
-                }
-            }, maxAttempts: null, delay: _ => TimeSpan.FromSeconds(15), ex => ex is HttpRequestException or TaskCanceledException,
-            beforeRetry: (i, e) =>
-                logger.LogWarning("Failed to fetch existing DNS record from Gandi HTTP API server, retrying (attempt {attempt}) {eType}: {message}", i + 2, e.GetType().Name, e.Message), ct);
+        try {
+            await Retrier.Attempt(
+                async _ => { // this retry is to handle the case where the service starts before the computer connects to the network on bootup, not where Gandi's API servers are down
+                    if ((await liveDns.Get(RecordType.A, configuration.Value.subdomain, ct))?.Values.First() is { } existingIpAddress) {
+                        try {
+                            selfWanAddress = IPAddress.Parse(existingIpAddress);
+                        } catch (FormatException) { }
+                    }
+                }, maxAttempts: null, delay: _ => TimeSpan.FromSeconds(3), ex => ex is not (OutOfMemoryException or GandiException { InnerException: ClientErrorException }),
+                beforeRetry: async (i, e) =>
+                    logger.LogWarning("Failed to fetch existing DNS record from Gandi HTTP API server, retrying (attempt {attempt}): {message}", i + 2, e.MessageChain()), ct);
 
-        logger.LogInformation("On startup, the {fqdn} DNS A record was pointing to {address}", configuration.Value.fqdn, selfWanAddress?.ToString() ?? "(nothing)");
+            logger.LogInformation("On startup, the {fqdn} DNS A record is pointing to {address}", configuration.Value.fqdn, selfWanAddress?.ToString() ?? "(nothing)");
 
-        while (!ct.IsCancellationRequested) {
-            await updateDnsRecordIfNecessary(ct);
-
-            if (configuration.Value.updateInterval > TimeSpan.Zero) {
-                await Tasks.Delay(configuration.Value.updateInterval, ct);
-            } else {
-                lifetime.StopApplication();
-                break;
+            if (configuration.Value.updateInterval > ONE_SHOT_MODE) {
+                logger.LogInformation("Checking for public IP address changes every {period}", configuration.Value.updateInterval);
             }
+
+            while (!ct.IsCancellationRequested) {
+                await updateDnsRecordIfNecessary(ct);
+
+                if (configuration.Value.updateInterval > ONE_SHOT_MODE) {
+                    await Tasks.Delay(configuration.Value.updateInterval, ct);
+                } else {
+                    logger.LogInformation(
+                        $"Exiting after one public IP address check. To continue running and checking for IP address changes repeatedly, set {nameof(Configuration.updateInterval)} to a {nameof(TimeSpan)} longer than {{zero}} in appsettings.json.",
+                        ONE_SHOT_MODE);
+                    lifetime.StopApplication();
+                    break;
+                }
+            }
+        } catch (GandiException e) when (e is GandiException.AuthException or { InnerException: ForbiddenException or NotAuthorizedException }) {
+            logger.LogError(
+                $"Auth error from Gandi. Please check the value of {nameof(Configuration.gandiAuthToken)} in appsettings.json and https://admin.gandi.net/organizations/account/pat to see if the Personal Access Token has expired.");
+            throw;
+        } catch (GandiException e) {
+            logger.LogError(e, "Error while communicating with the Gandi LiveDNS API.");
+            throw;
         }
     }
 
@@ -96,6 +116,9 @@ public class DynamicDnsServiceImpl(ILiveDns liveDns, ISelfWanAddressClient stun,
                 await liveDns.Set(new DnsRecord(RecordType.A, configuration.Value.subdomain, configuration.Value.dnsRecordTimeToLive, [currentIPAddress.ToString()]), ct);
             } catch (GandiException e) {
                 logger.LogError(e, "Failed to update DNS record for {fqdn} to {newAddr} due to DNS API server error", configuration.Value.fqdn, currentIPAddress);
+                if (e is GandiException.AuthException or { InnerException: ForbiddenException or NotAuthorizedException }) {
+                    throw;
+                }
             }
         } else {
             logger.LogInformation("Dry run mode, not updating {fqdn} to {newAddr}. To actually make DNS changes, change {dryRun} from true to false in appsettings.json.", configuration.Value.fqdn,
